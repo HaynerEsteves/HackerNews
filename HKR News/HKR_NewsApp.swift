b169b540a0e3edc125c9d02d389b326f43d050b3
HaynerEsteves <Hayner>
2023-09-27 14:45:22 -0300

    Initial Commit

---
+++ HKR News/HKR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKR_NewsApp.swift
+//  HKR News
+//
+//  Created by Hayner Esteves on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HKR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
